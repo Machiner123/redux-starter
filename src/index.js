e0a6eeba097c54d27eaf587bdc89a8c0805f1b52
unknown <igoratakhanov@gmail.com>
2018-01-11 16:01:56 -0500

    Added state and props, began search bar component.

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import SearchBar from './components/search_bar'
+
+const API_KEY = "AIzaSyCaKRXI91k2HLNq8BOE5IxRKAbWesO-I5U"
+// Create element to render
+const App = ()=>{
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+}
+
+// Render element on DOM
+
+ReactDOM.render(<App />, document.querySelector('.container'))
