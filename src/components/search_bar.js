ea773d6ca7e3388a72f6b60c5ee9da2fef89f76e
igor atakhanov <igoratakhanov@gmail.com>
2018-01-16 11:46:24 -0500

    Fleshed out visiual components.
    Todo: fix onSearchTermChange

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,18 +9,23 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <div>
+      <div className="search-bar">
 
         <input
           value={this.state.term}
-          onChange={event => this.setState({ term: event.target.value})}
+          onChange={this.onInputChange(event.target.value)}
         />
 
       </div>
     )
   }
 
-
+  onInputChange(term) {
+    this.setState({
+      term: term
+    })
+    this.props.onSearchTermChange(term)
+  }
 }
 
 export default SearchBar;
